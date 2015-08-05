fa32f29d1d4df4bf8712b866cc7ba25a7af8a4e9
Katochimoto <bufpost@yandex.ru>
2015-08-05 17:59:34 +0300

    откат

--- plugin.js
+++ plugin.js
@@ -396,10 +396,6 @@
                 delete this._pastefileDNDHover;
             }
 
-            if (this.status !== 'ready') {
-                return;
-            }
-
             var isMaximize = (this.getCommand('maximize').state === CKEDITOR.TRISTATE_ON);
             var dropContext;
 
