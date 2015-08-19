2bbf410085972b6e5c56c47eabc884d75985d361
Katochimoto <bufpost@yandex.ru>
2015-08-19 20:28:25 +0300

    не показывать паранджу

--- plugin.js
+++ plugin.js
@@ -680,8 +680,6 @@
 
     ClipboardDataIterator.prototype.REG_BREAK_TYPE = /text\/(rtf|plain)/;
 
-    ClipboardDataIterator.prototype.REG_TEXT_TYPE = /text\/(rtf|plain|html)/;
-
     /**
      * Поиск файлов/картинок
      * @returns {{ inline: boolean, file: boolean }}
@@ -735,7 +733,7 @@
             if (CKEDITOR.fileTools.isTypeSupported(item, this.REG_IMAGE_TYPE)) {
                 data.inline = true;
 
-            } else if (!CKEDITOR.fileTools.isTypeSupported(item, this.REG_TEXT_TYPE)) {
+            } else if (item.kind === 'file') {
                 data.file = true;
             }
 
